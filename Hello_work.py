fd7af28ef16ed12c7f73bb2b8563f79f1d86f16a
littleone <nikita311098@mail.ru>
2022-05-08 17:35:46 +0300

    dd

--- Hello_work.py
+++ Hello_work.py
@@ -1,4 +1,10 @@
+import argparse
+par = argparse.ArgumentParser()
+par.add_argument("a",type = int, help = "Num 1")
+par.add_argument("b",type = int, help = "Num 2")
+par.add_argument("-v", "--numer", action="count", default= 0)
+x = par.parse_args()
+ans = x.a+x.b
 print('Hi')
-x = 5 + 10
-print(x)
+print(ans)
 print('New Line')
