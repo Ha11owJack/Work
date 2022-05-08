b7dac32519af640c5eceba363721d0e492f166c8
littleone <nikita311098@mail.ru>
2022-05-08 21:57:54 +0300

    t_s

--- Hello_work.py
+++ Hello_work.py
@@ -1,10 +1,18 @@
 import argparse
+import sqlite3
+import random 
 par = argparse.ArgumentParser()
-par.add_argument("a",type = int, help = "Num 1")
-par.add_argument("b",type = int, help = "Num 2")
-par.add_argument("-v", "--numer", action="count", default= 0)
+par.add_argument("a",type = int, help = "Num str")
+par.add_argument("k",type = str, help = "Name table")
 x = par.parse_args()
-ans = x.a+x.b
-print('Hi')
-print(ans)
-print('New Line')
+K = x.k
+Z = x.a
+Table1 = sqlite3.connect('server.Tablet1')
+sql = Table1.cursor()
+sql.execute("CREATE TABLE IF NOT EXISTS %s (id INT,N INT)" %K)
+Table1.commit()
+for i in range(Z):
+    U = random.randint(1,100)
+    sql.execute("INSERT INTO %s VALUES(?,?)" %K, (i+1,U))
+sql.execute("SELECT * FROM %s" %K)
+print(sql.fetchall())
