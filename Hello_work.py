582d00cb8523cfed5495412d1e25b11fd6fe90aa
Jack <nik.shishov.9898@mail.ru>
2022-05-09 19:37:15 +0300

    Перенос и изменение

--- Hello_work.py
+++ Hello_work.py
@@ -1,18 +1,29 @@
-import argparse
+import argparse #Парсер
 import sqlite3
 import random 
-par = argparse.ArgumentParser()
-par.add_argument("a",type = int, help = "Num str")
-par.add_argument("k",type = str, help = "Name table")
-x = par.parse_args()
-K = x.k
-Z = x.a
-Table1 = sqlite3.connect('server.Tablet1')
-sql = Table1.cursor()
-sql.execute("CREATE TABLE IF NOT EXISTS %s (id INT,N INT)" %K)
-Table1.commit()
-for i in range(Z):
-    U = random.randint(1,100)
-    sql.execute("INSERT INTO %s VALUES(?,?)" %K, (i+1,U))
-sql.execute("SELECT * FROM %s" %K)
-print(sql.fetchall())
+import os
+par = argparse.ArgumentParser() # Создание парсера
+subparser = par.add_subparsers(dest = "command") # Объявление субпарсера 
+Sub1 = subparser.add_parser('show', help = 'Table') # Создание субпарсера показать список таблиц
+Sub2 = subparser.add_parser('create', help = 'Create Table')# Создание субпарсера создание таблиц
+Sub2.add_argument("a",type = int, help = "Num str") # Аргумент субпарсера 2 (число)
+Sub2.add_argument("k",type = str, help = "Name table")# Аргмент субпарсера 2 (строка)
+x = par.parse_args() # Присвоение всеё информации парсера
+if x.command == "create": # проверка на слово
+    K = x.k # присвоение переменной
+    Z = x.a 
+    Table1 = sqlite3.connect('server.Tablet1') 
+    sql = Table1.cursor()
+    sql.execute("CREATE TABLE IF NOT EXISTS %s (id INT,N INT)" %K) # Создание произвольной таблицы
+    Table1.commit()
+    for i in range(Z): #Создание таблицы размером Z
+        U = random.randint(1,100)# рандомное число
+        sql.execute("INSERT INTO %s VALUES(?,?)" %K, (i+1,U))# Вставка в произвольную таблицу
+    sql.execute("SELECT * FROM %s" %K)# Выбрать всё из таблицы
+    print(sql.fetchall())# Вывод таблицы
+elif x.command == "show":# проверка на слово
+    user = os.getlogin()# имя юзера(для папки)
+    Table1 = sqlite3.connect('/home/%s/Work/server.Tablet1' %user, uri = True)# поиск таблицы в папке
+    sql = Table1.cursor()
+    sql.execute("SELECT name FROM sqlite_master WHERE type = 'table';") # выполнение команды поиска таблиц
+    print(sql.fetchall())#вывод таблиц
