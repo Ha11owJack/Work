e87a4b08b811586cf36b83521f6391179d0fce78
littleone <nikita311098@mail.ru>
2022-05-13 15:36:45 +0300

    7

--- Hello_work.py
+++ Hello_work.py
@@ -1,44 +1,67 @@
 import argparse
 import sqlite3
 import random 
-import os
+
+
 def multi():
-    Table1 = sqlite3.connect('%s.db' %TABLE.name) 
-    sql = Table1.cursor()
+    Table = sqlite3.connect('%s.db' 
+                            %Table_work.name) 
+    sql = Table.cursor()
     Table_Length = 20
-    for i in range (TABLE.num):
-        Name_Tab_s = 'Table' + str(i+1)
-        sql.execute("CREATE TABLE IF NOT EXISTS %s (id INT,N INT)" %Name_Tab_s)                 #  Создает бд с произвольным кол-вом таблиц и произвольной длиной таблиц
-        Table1.commit()
+    for i in range(Table_work.num):
+        Name_Tabs = Table_work.name + str(Table_Length)
+        sql.execute("CREATE TABLE IF NOT EXISTS %s (id INT,N INT)" 
+                    %Name_Tabs)                                                                    #  Создает бд с произвольным кол-вом таблиц и произвольной длиной таблиц
+        Table.commit()
         for i in range(Table_Length):
             Random_number = random.randint(1,100)
-            sql.execute("INSERT INTO %s VALUES(?,?)" %Name_Tab_s, (i+1,Random_number))# 
-        sql.execute("SELECT * FROM %s" %Name_Tab_s)
-        print('\n'+Name_Tab_s) 
+            sql.execute("INSERT INTO %s VALUES(?,?)" 
+                        %Name_Tabs, (i+1,Random_number))
+        sql.execute("SELECT * FROM %s" 
+                    %Name_Tabs)
+        print('\n'+Name_Tabs) 
         print(sql.fetchall())  
         print('\n\n\n') 
+
+
 def create():
-    Table1 = sqlite3.connect('%s.db' %TABLE.name) 
-    sql = Table1.cursor()
+    Table = sqlite3.connect('%s.db' 
+                            %Table_work.name) 
+    sql = Table.cursor()
     Table_Length = 20
-    sql.execute("CREATE TABLE IF NOT EXISTS %s (id INT,N INT)" %TABLE.TAB)                 #  Создает бд c таблицей произвольного имени
+    sql.execute("CREATE TABLE IF NOT EXISTS %s (id INT,N INT)" 
+                %Table_work.TAB)                                                                     #  Создает бд c таблицей произвольного имени
     for i in range(Table_Length):
-        Random_number = random.randint(1,100)
-        sql.execute("INSERT INTO %s VALUES(?,?)" %TABLE.TAB, (i+1,Random_number))# 
-    sql.execute("SELECT * FROM %s" %TABLE.TAB)
-    print('\n'+TABLE.TAB) 
+        Random_number = random.randint(1, 100)
+        sql.execute("INSERT INTO %s VALUES(?,?)" 
+                    %Table_work.TAB, 
+                    (i+1,Random_number))
+    sql.execute("SELECT * FROM %s" 
+                %Table_work.TAB)
+    print('\n'+Table_work.TAB) 
     print(sql.fetchall())  
     print('\n\n\n') 
 
+
 par = argparse.ArgumentParser()
 subparser = par.add_subparsers()  
-CREATE_Y_T = subparser.add_parser('create', help = 'Create Table')
-CREATE_Y_T.add_argument("name",type = str , help = "Name of db that you need to connect")
-CREATE_Y_T.add_argument("TAB",type = str , help = "Name of table")
-Cr_m_t = subparser.add_parser('multi', help = 'Create Table')
-Cr_m_t.add_argument("name",type = str , help = "Name of db that you need to connect")
-Cr_m_t.add_argument("num",type = int, help = "Number of Tables you need to create")
-Cr_m_t.set_defaults(func=multi)
-CREATE_Y_T.set_defaults(func=create)
-TABLE = par.parse_args() 
-TABLE.func()
+Create_one = subparser.add_parser('create', 
+                                    help = 'Create Table')
+Create_one.add_argument("-n", "--name", 
+                            type = str, 
+                            help = "Name of db that you need to connect")
+Create_one.add_argument("-t", "--TAB", 
+                            type = str, 
+                            help = "Name of table")
+Create_multi = subparser.add_parser('multi', 
+                                    help = 'Create Table')
+Create_multi.add_argument("-n", "--name", 
+                            type = str, 
+                            help = "Name of db that you need to connect")
+Create_multi.add_argument("-num", "--num",
+                            type = int, 
+                            help = "Number of Tables you need to create")
+Create_multi.set_defaults(func=multi)
+Create_one.set_defaults(func=create)
+Table_work = par.parse_args() 
+Table_work.func()
