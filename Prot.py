ccd0de5ee5ceeb9adc453b8688a66024b7c5fde4
littleone <nikita311098@mail.ru>
2022-05-20 20:41:33 +0300

    Обяхательные/непозиционные аргументы + константы

--- Prot.py
+++ Prot.py
@@ -4,21 +4,21 @@ import random
 import os
 def parser():
     high_parser = argparse.ArgumentParser()
+    high_parser.add_argument("-n",required = True,
+                            type = str, 
+                            help = "Name of db that you need to connect")
     subparser = high_parser.add_subparsers()  
     create_multi = subparser.add_parser('multi', 
                                     help = 'Create Table')
     show_table = subparser.add_parser('show', 
                                     help = 'show the Table')                               
-    high_parser.add_argument("-n", 
-                            type = str, 
-                            help = "Name of db that you need to connect")
-    create_multi.add_argument("-l",
+    create_multi.add_argument("-l", nargs = '?', const = 5,
                             type = int, 
                             help = "High of table")
-    create_multi.add_argument("table", 
+    create_multi.add_argument("-t", "--table", required = True, 
                             type = str, 
                             help = "Name of table")
-    create_multi.add_argument("number",
+    create_multi.add_argument("-i","--number", nargs='?', const=5, 
                             type = int, 
                             help = "Number of Tables you need to create")
     show_table.set_defaults(func=show)
@@ -38,20 +38,23 @@ def multi(parser):
     table = sqlite3.connect('%s.db' 
                             %parser.n) 
     sql = table.cursor()
-    for i in range(parser.number):
-        vertion = "_" + parser.table + "_" + str(i+1)
-        name_tabs = parser.n + str(parser.l) + vertion
-        sql.execute("CREATE TABLE IF NOT EXISTS %s (id INT,N INT)" 
-                    %name_tabs)                                                                    #  Создает бд с произвольным кол-вом таблиц и произвольной длиной таблиц
-        table.commit()
-        for i in range(parser.l):
-            random_number = random.randint(1,100)
-            sql.execute("INSERT INTO %s VALUES(?,?)" 
-                        %name_tabs, (i+1,random_number))
-        sql.execute("SELECT * FROM %s" 
-                    %name_tabs)
-        print('\n'+name_tabs) 
-        print(sql.fetchall())  
-        print('\n\n') 
+    if parser.number <= 0 or parser.l <= 0:
+        print("Неверно введены аргументы")
+    else:
+        for i in range(parser.number):
+            vertion = "_" + parser.table + "_" + str(i+1)
+            name_tabs = parser.n + str(parser.l) + vertion
+            sql.execute("CREATE TABLE IF NOT EXISTS %s (id INT,N INT)" 
+                        %name_tabs)                                                                    #  Создает бд с произвольным кол-вом таблиц и произвольной длиной таблиц
+            table.commit()
+            for i in range(parser.l):
+                random_number = random.randint(1,100)
+                sql.execute("INSERT INTO %s VALUES(?,?)" 
+                            %name_tabs, (i+1,random_number))
+            sql.execute("SELECT * FROM %s" 
+                        %name_tabs)
+            print('\n'+name_tabs) 
+            print(sql.fetchall())  
+            print('\n\n') 
     print("Replace id\n")
 parser()
