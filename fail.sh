46724fe0612a37a495953ca85ef7bd50a62d12d0
Ha11owJack <103958197+Ha11owJack@users.noreply.github.com>
2022-06-23 17:58:32 +0300

    Add files via upload

--- fail.sh
+++ fail.sh
@@ -1,34 +1,29 @@
 #!/bin/bash
 cd
-#echo "Введите путь до папок с файлами: "
+echo "Введите название файла: "
 #read FILE
-
+#read Path
+read Name
 
 if [[ ! -d "/etc/apache2" ]]; then
 	echo "Скачиваю пакет Apache"
-	sudo apt-get install apache2
-#	git init
-#	git add *
-#	git commit -m "Бэш работает"
-#	git remote add origin http://10.20.13.4:3000/Ha11owJack2.0/reposit.git
-#	git push -u origin master
+	sudo apt install apache2
 else
     echo "Пакет Apache Установелен"
 fi
+#if [[ ! -d "srv/$Name/file" ]]; then
+#	echo "Создаем папку репозитория"
+#	sudo mkdir -p /srv/$Name/file
+#else
+#    echo "Папка уже существует"
+#fi
 
-
-if [[ ! -d "srv/repo/file" ]]; then
-	echo "Создаем папку репозитория"
-	sudo mkdir -p /srv/repo/file
-else
-    echo "Папка уже существует"
-fi
-
-sudo ln -s /srv/repo /var/www/html/
-sudo sed -i "s/#ApacheMod on/AstraMod off/g" /etc/apache2/apache2.conf
-# echo "deb http://IP/repo name main contrib non-free" >> /etc/apt/sources.list
-
-echo "Перезагрузка сервера"
-sudo systemctl restart apache2
-echo "Сервер перезагрузился"
-sudo apt-get install
+#sudo chmod -R 755 /var/www/$Name
+#sudo sed -i "s/# AstraMode on/AstraMode off/g" /etc/apache2/apache2.conf
+#sudo sed -i '$Name' /etc/apache2/sites-enabled/000-default.conf
+sudo sed -i '2i<Directory /var/www/WORD_TO_REPLACE> \n\tOptions Indexes MultiViews FollowSymLinks \n\tAllowOverride None \n\tOrder Deny,Allow \n\tRequire all granted \n</Directory>' /etc/apache2/sites-enabled/000-default.conf
+sudo sed -i 's/WORD_TO_REPLACE/($Name;n_)/g' /etc/apache2/sites-enabled/000-default.conf
+sudo sed -i 's|DocumentRoot /var/www/html|DocumentRoot /var/www/{$Name}|g' /etc/apache2/sites-enabled/000-default.conf
+#echo "Перезагрузка сервера"
+#sudo systemctl restart apache2
+#echo "Сервер перезагрузился"
